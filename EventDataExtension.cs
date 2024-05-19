dd1d6c92ad7cf5af3f1cca69f5e70bdea89adaa3
abduttawab <abduttawabcse@gmail.com>
2024-05-19 22:05:34 +0600

    init

---
+++ EventDataExtension.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureEventHub
+{
+    public static class EventDataExtension
+
+    {
+
+        public static void SetEventName(this EventData eventData, string eventName)
+        {
+
+            SetPropertyValue(eventData, "eventName", eventName);
+
+        }
+
+        private static void SetPropertyValue(EventData eventData, string propertyName, object value)
+        {
+            eventData.Properties[propertyName] = value;
+        }
+
+        public static void SetReceivedAt(this EventData eventData, long receivedAt)
+        {
+            SetPropertyValue(eventData, "receivedAt", receivedAt);
+        }
+
+    }
+}
---
+++ EventHubEventSender.cs
@@ -0,0 +1,49 @@
+﻿
+using Microsoft.ServiceBus.Messaging;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AzureEventHub
+{
+    public class EventHubEventSender
+    {
+        string connectionString = "—- Get key from azure queue ->Shared access key------  From APP SET - —";
+        string eventHubName = "—-Event hub name-------  From APP SET - ";
+
+        public async Task SendEventAsync(JArray events, string deviceId)
+        {
+
+            if (events.Count > 0)
+            {
+
+                var client = EventHubClient.CreateFromConnectionString(connectionString, eventHubName);
+
+                foreach (dynamic eventObj in events)
+                {
+                    var json = eventObj.ToString(Newtonsoft.Json.Formatting.None);
+                    EventData eventData = new EventData(Encoding.UTF8.GetBytes(json))
+                    {
+
+                        PartitionKey = deviceId
+                    };
+
+                    eventData.SetEventName((string)eventObj.eventName);
+                    eventData.SetReceivedAt((long)eventObj.receivedAt);
+
+                    await client.SendAsync(eventData);
+                }
+
+
+
+            }
+
+
+        }
+    }
+}
